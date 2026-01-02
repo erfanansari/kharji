2ead685cec94987ec3c4711e03b33d784fb4020b
Erfan Ansari <dev.erfanansari@gmail.com>
2026-01-02 15:44:08 +0330

    feat: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next"
 import { Geist, Vazirmatn } from "next/font/google";
 import "./globals.css";
 
@@ -38,6 +39,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${persianFont.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
